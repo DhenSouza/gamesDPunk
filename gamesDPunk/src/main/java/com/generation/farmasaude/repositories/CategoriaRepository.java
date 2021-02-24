6fc125634785001510161288cb81e6d06e6e503c
ShenSouza <ds052798ts@gmail.com>
2021-02-24 11:30:57 -0300

    Upando Controller, model, repository de produto e categoria

---
+++ gamesDPunk/src/main/java/com/generation/farmasaude/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.generation.farmasaude.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.farmasaude.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+	public List<Categoria> findAllByDescricaoContainingIgnoreCase(String descricao);
+}
