e2421a06818faf50342a4cc54209c5f40b5884e3
ShenSouza <ds052798ts@gmail.com>
2021-02-24 09:11:55 -0300

    Projeto inicial

---
+++ gamesDPunk/src/main/java/com/generation/farmasaude/GamesDPunkApplication.java
@@ -0,0 +1,13 @@
+package com.generation.farmasaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesDPunkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesDPunkApplication.class, args);
+	}
+
+}
